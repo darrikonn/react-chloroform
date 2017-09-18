dbd9936db96e81caa62f9de288c565aa70e6fe98
Darri Steinn Konradsson <darri15@hotmail.com>
2017-09-18 22:02:15 +0000

    Add umd es commonjs build

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+var env = process.env.NODE_ENV
+var config = {
+  name: 'ReactChloroform',
+  external: [
+    'react',
+  ],
+  globals: {
+    'react': 'React',
+  },
+  output: {
+    format: 'umd',
+  },
+  plugins: [
+    babel({
+      exclude: '**/node_modules/**'
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: { './node_module/invariant.js': ['default'] }
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
