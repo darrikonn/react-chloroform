b88be28955dd4501aca73eadcf64695982dbf3a9
Darri Steinn Konradsson <darri15@hotmail.com>
2017-10-15 17:33:01 +0000

    Simplify radio button to functional component and to use connect

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Control from './Control';
+import {updateValue} from '../actions/controls';
+import {connect} from '../store';
 
-class RadioButton extends Control {
-  static propTypes = {
-    className: PropTypes.string,
-    id: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    style: PropTypes.string,
-    value: PropTypes.string.isRequired,
-  }
+const RadioButton = ({id, name, value, className, style, updateValue, checked}) =>
+  <input
+    type="radio"
+    id={id}
+    className={className}
+    style={style}
+    name={name}
+    value={value}
+    checked={checked === value}
+    onChange={(e) => updateValue(name, e.target.value)}
+  />;
 
-  render() {
-    const {id, name, value, className, style} = this.props;
-
-    return (
-      <input
-        type="radio"
-        id={id}
-        className={className}
-        style={style}
-        name={name}
-        value={value}
-        checked={this._getValue() === value}
-        onChange={(e) => this._onChange(e.target.value)}
-      />
-    );
-  }
+RadioButton.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  style: PropTypes.string,
+  value: PropTypes.string.isRequired,
 }
 
-export default RadioButton;
+const mapStateToProps = ({controls}, props) => ({
+  checked: controls.get(props.name),
+});
+
+const mapDispatchToProps = {
+  updateValue,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RadioButton);
