5378eb7a78a4cf4625305fa2fe88a2c9d52be24f
Darri Steinn Konradsson <darri15@hotmail.com>
2017-10-15 17:31:07 +0000

    Add create store function

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,21 @@
-export default (reducer) => {
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+const createStore = (reducer) => {
   let state;
   let listeners = [];
 
   const getState = () => state;
 
-  const dispatch = (action) => {
+  const dispatch = action => {
     state = reducer(state, action);
-    listeners.forEach((listener) => listener());
+    listeners.forEach(listener => listener());
   };
 
-  const subscribe = (listener) => {
+  const subscribe = listener => {
     listeners.push(listener);
     return () => {
-      listeners = listeners.filter((l) => l !== listener);
+      listeners = listeners.filter(l => l !== listener);
     };
   };
 
