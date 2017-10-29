5bb50b03e189c6678867122366ae32402b6a6b52
Darri Steinn Konradsson <darri15@hotmail.com>
2017-10-29 18:45:27 +0000

    Add action creator

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+const camelCase = string => string.toLowerCase().replace(/(_\w)/g, m => m[1].toUpperCase());
+
+const actionCreator = ({type, payload}) => dispatch =>
+  dispatch({
+    type,
+    payload,
+  });
+
+const createActions = actions =>
+  Object.keys(actions).reduce(
+    (createdActions, type) => ({
+      ...createdActions,
+      [camelCase(type)]: (...args) => actionCreator({type, payload: actions[type](...args)}),
+    }),
+    {},
+  );
+
+export default createActions;
