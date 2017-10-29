7fb277249eb06eeca1dc40cc6c2c57a03c3e2482
Darri Steinn Konradsson <darri15@hotmail.com>
2017-10-29 18:46:33 +0000

    Add submit pending states reducer

--- src/store/reducers/states.js
+++ src/store/reducers/states.js
@@ -1,12 +1,24 @@
 import Immutable from 'immutable';
 
-import {SET_PENDING} from '../action-types';
+import {SET_PENDING, SET_SUBMITTING, SET_SUBMIT_FAILED, RESET_STATES} from '../action-types';
 
-export default (state = Immutable.Map(), action) => {
+const initialState = Immutable.fromJS({
+  pending: false,
+  submitting: false,
+  submitFailed: false,
+});
+
+export default (state = initialState, action) => {
   const {payload} = action;
   switch (action.type) {
     case SET_PENDING:
-      return state.set(payload.name, payload.pending);
+      return state.setIn([payload.name, 'pending'], payload.pending);
+    case SET_SUBMITTING:
+      return state.setIn([payload.name, 'submitting'], payload.pending);
+    case SET_SUBMIT_FAILED:
+      return state.setIn([payload.name, 'submitFailed'], payload.pending);
+    case RESET_STATES:
+      return initialState;
     default:
       return state;
   }
