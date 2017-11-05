e1e3aef8f3a3798744d99b53c2822fd9c4a8ff05
Darri Steinn Konradsson <darri15@hotmail.com>
2017-11-05 01:08:35 +0000

    Add action types

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,7 +1,8 @@
 export const INITIALIZE_STATE = 'INITIALIZE_STATE';
 export const RESET_FORM = 'RESET_FORM';
-export const RESET_STATES = 'RESET_STATES';
+export const RESET_SUBMIT = 'RESET_SUBMIT';
+export const SET_ERRORS = 'SET_ERRORS';
 export const SET_PENDING = 'SET_PENDING';
 export const SET_SUBMITTING = 'SET_SUBMITTING';
 export const SET_SUBMIT_FAILED = 'SET_SUBMIT_FAILED';
-export const UPDATE_VALUE = 'UPDATE_VALUE';
+export const SET_VALUE = 'SET_VALUE';
