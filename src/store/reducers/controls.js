a4651c5c12ca10d12f2a4de480e944658e3bff8c
Darri Steinn Konradsson <darri15@hotmail.com>
2017-11-05 01:09:00 +0000

    Add pending and errors control reducer

--- src/store/reducers/controls.js
+++ src/store/reducers/controls.js
@@ -1,15 +1,44 @@
 import Immutable from 'immutable';
 
-import {UPDATE_VALUE, INITIALIZE_STATE} from '../action-types';
+import {SET_VALUE, SET_ERRORS, SET_PENDING, INITIALIZE_STATE} from '../action-types';
 
 export default (state = Immutable.Map(), action) => {
   const {payload} = action;
   switch (action.type) {
     case INITIALIZE_STATE:
-      return state.merge(payload.state);
-    case UPDATE_VALUE:
-      return state.set(payload.name, payload.value);
+      return state.merge(
+        Immutable.fromJS(
+          Object.keys(payload.state).reduce(
+            (obj, k) => ({
+              ...obj,
+              [k]: {value: payload.state[k]},
+            }),
+            {},
+          ),
+        ),
+      );
+    case SET_ERRORS:
+      return state.setIn([payload.name, 'errors'], Immutable.fromJS(payload.errors));
+    case SET_PENDING:
+      return state.setIn([payload.name, 'pending'], payload.pending);
+    case SET_VALUE:
+      return state.setIn([payload.name, 'value'], payload.value);
     default:
       return state;
   }
+};
+
+export const getValue = (state, name) => state.getIn([name, 'value']);
+
+export const getValues = state => state.map(model => model.get('value'));
+
+export const getErrors = state => state.map(model => model.get('errors'));
+
+export const hasError = state => state.filter(model => model.get('errors')).size > 0;
+
+export const getErrors2 = state => {
+  return state.reduce((lis, key) => {
+    const errors = state.getIn([key, 'errors']);
+    return [...lis, ...(errors ? errors : lis)];
+  }, []);
 };
